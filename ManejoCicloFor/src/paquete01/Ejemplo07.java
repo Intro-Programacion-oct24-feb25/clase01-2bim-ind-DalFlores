3b6083fccfe3c435c66a8f67c36aee6c9bb019bb
Dalton Flores <daltonflores@gmail.com>
2024-12-05 11:05:27 -0500

    Suma

--- ManejoCicloFor/src/paquete01/Ejemplo07.java
+++ ManejoCicloFor/src/paquete01/Ejemplo07.java
@@ -12,21 +12,30 @@ package paquete01;
 public class Ejemplo07 {
     public static void main(String[] args) {
         int operacion;
+        int suma;
         String mensajeFinal = "";
+        String mensajeSuma = "";
         for (int i = 1; i <= 10; i++) { // for 01
             // System.out.printf("Tabla de multiplicar del número %d\n", i);
+            mensajeSuma = String.format("%sTabla de sumar del numero %d\n",
+                    mensajeSuma,i);
             mensajeFinal = String.format("%sTabla de multiplicar del número %d\n", 
                     mensajeFinal, i);
             for (int contador = 1; contador <= 12; contador++) {
                 operacion = i * contador;
+                suma = i + contador;
                 // System.out.printf("%d x %d = %d\n", i, contador, operacion);
+                mensajeSuma = String.format("%s%d + %d = %d\n", 
+                        mensajeSuma, i, contador, suma);
+                
                 mensajeFinal = String.format("%s%d x %d = %d\n", 
                         mensajeFinal, i, contador, operacion);
             }
             // System.out.println("\n");
             mensajeFinal = String.format("%s\n", mensajeFinal);
+            mensajeSuma = String.format("%s\n", mensajeSuma);
         } // termina for 01
         
-        System.out.printf("%s\n", mensajeFinal);
+        System.out.printf("%s%s\n", mensajeFinal, mensajeSuma);
     }
 }
