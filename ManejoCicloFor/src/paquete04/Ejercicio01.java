b9e0014112c6f89e28b0815ef19e07d9429ce0bf
Dalton Flores <daltonflores@gmail.com>
2024-12-05 11:53:31 -0500

    Taller

---
+++ ManejoCicloFor/src/paquete04/Ejercicio01.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete04;
+
+/**
+ *
+ * @author utpl
+ */
+public class Ejercicio01 {
+
+    public static void main(String[] args) {
+        
+        int operacion;
+        String cadena = "";
+        for (int i = 30; i >= 10; i--) {
+            cadena = String.format("%s%d", cadena,i);
+            for (int o = 2; o <= 4; o++) {
+                operacion = i * o;
+                cadena = String.format("%s-%d", cadena,
+                         operacion);
+            }
+            cadena = String.format("%s\n", cadena);
+        }
+         System.out.printf("%s\n", cadena);
+    }
+}
